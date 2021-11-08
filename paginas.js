173ec773ab2e985ebadb65e016a9ab565aa6ec17
Juan Pablo <jballes89830@universidadean.edu.co>
2021-11-08 15:20:48 -0500

    algoritmo de imagenes creado

---
+++ paginas.js
@@ -0,0 +1,46 @@
+const contenedor = document.getElementById("contenedor");
+
+const crearPaginas = (n, direction, autor) => {
+    const div = document.createElement("DIV");
+    const sombra = document.createElement("DIV");
+    const img = document.createElement("IMG");
+
+    div.classList.add("divImagen");
+    sombra.classList.add(`sombra${direction}`);
+    img.setAttribute("src", `comic${autor}/page${n}.jpg`);
+    img.setAttribute("alt", `Imagen del comic`);
+
+    div.appendChild(sombra);
+    div.appendChild(img);
+
+    contenedor.appendChild(div);
+}
+
+const evaluarLR = d => {
+    if (d % 2 === 0) {
+        return "R"
+    } else {
+        return "L";
+    }
+}
+
+const ponerPaginas = nombre => {
+    contenedor.innerHTML = `
+    <div style="text-align: center; display: flex; align-items: center; justify-content: center; height: 100%; color: white;">
+		<h1>Has click en los botones para pasar de página</h1>
+	</div>
+    `;
+
+    for (let i = 1; i <= 6; i++) {
+        crearPaginas(i, evaluarLR(i), nombre);
+    }
+
+    contenedor.innerHTML += `
+    <div style="text-align: center; display: flex; align-items: center; justify-content: center; height: 100%; color: white;">
+		<h1>Has llegado al final</h1>
+	</div>
+    `;
+
+}
+
+ponerPaginas("Chevy");
