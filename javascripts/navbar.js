6d9539aa8d5f5614df011862c5185fbdbb67d49e
Juan-Pablo-EAN <jballes89830@universidadean.edu.co>
2021-11-22 15:35:26 -0500

    mejoras en el diseño

---
+++ javascripts/navbar.js
@@ -0,0 +1,12 @@
+const navBarr = document.getElementById("navBarr");
+const barras = document.querySelector(".barras");
+
+barras.addEventListener("click", () => {
+    if(navBarr.className == "visible"){
+        navBarr.className = "invisible";
+        barras.style.color = "white";
+    } else {
+        navBarr.className = "visible";
+        barras.style.color = "#282645";
+    }
+});
