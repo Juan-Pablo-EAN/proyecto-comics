6f98ca1e7753a1645dc7e94e10aacbd0a1f9b73a
Juan-Pablo-EAN <jballes89830@universidadean.edu.co>
2021-11-23 14:07:03 -0500

    scroll

--- javascripts/navbar.js
+++ javascripts/navbar.js
@@ -14,16 +14,18 @@ barras.addEventListener("click", () => {
 let alto = navBarr.offsetTop;
 window.addEventListener("scroll", () => {
     if (location.href.includes("quienessomos") === false) {
-        if (window.scrollY >= alto && window.screen.availWidth > 600) {
-            navBarr.style.position = "fixed";
-            navBarr.style.top = "0";
-            navBarr.style.left = "0";
-            navBarr.style.width = "97.8%";
-            document.querySelector("section").style.marginTop = "50px";
-        } else {
-            navBarr.style.position = "relative";
-            navBarr.style.width = "auto";
-            document.querySelector("section").style.marginTop = "0px";
+        if (window.screen.availWidth > 600) {
+            if (window.scrollY >= alto) {
+                navBarr.style.position = "fixed";
+                navBarr.style.top = "0";
+                navBarr.style.left = "0";
+                navBarr.style.width = "97.8%";
+                document.querySelector("section").style.marginTop = "50px";
+            } else {
+                navBarr.style.position = "relative";
+                navBarr.style.width = "auto";
+                document.querySelector("section").style.marginTop = "0px";
+            }
         }
     }
 });
