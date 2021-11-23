e1d55efafd5aa3c1a14201b696ab81bedb1acf85
Juan-Pablo-EAN <jballes89830@universidadean.edu.co>
2021-11-23 13:17:25 -0500

    quienes somos diseñado

--- javascripts/navbar.js
+++ javascripts/navbar.js
@@ -2,7 +2,7 @@ const navBarr = document.getElementById("navBarr");
 const barras = document.querySelector(".barras");
 
 barras.addEventListener("click", () => {
-    if(navBarr.className == "visible"){
+    if (navBarr.className == "visible") {
         navBarr.className = "invisible";
         barras.style.color = "white";
     } else {
@@ -13,15 +13,17 @@ barras.addEventListener("click", () => {
 
 let alto = navBarr.offsetTop;
 window.addEventListener("scroll", () => {
-    if(window.scrollY >= alto && window.screen.availWidth > 600){
-        navBarr.style.position = "fixed";
-        navBarr.style.top = "0";
-        navBarr.style.left = "0";
-        navBarr.style.width = "97.8%";
-        document.querySelector("section").style.marginTop = "50px";
-    } else {
-        navBarr.style.position = "relative";
-        navBarr.style.width = "auto";
-        document.querySelector("section").style.marginTop = "0px";
+    if (location.href.includes("quienessomos") === false) {
+        if (window.scrollY >= alto && window.screen.availWidth > 600) {
+            navBarr.style.position = "fixed";
+            navBarr.style.top = "0";
+            navBarr.style.left = "0";
+            navBarr.style.width = "97.8%";
+            document.querySelector("section").style.marginTop = "50px";
+        } else {
+            navBarr.style.position = "relative";
+            navBarr.style.width = "auto";
+            document.querySelector("section").style.marginTop = "0px";
+        }
     }
 });
