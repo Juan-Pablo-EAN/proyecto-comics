e64023efe175470f25fa424e3cb531f496ce92dd
Juan-Pablo-EAN <jballes89830@universidadean.edu.co>
2021-11-23 10:43:28 -0500

    efecto barra de navegacion

--- javascripts/navbar.js
+++ javascripts/navbar.js
@@ -10,3 +10,18 @@ barras.addEventListener("click", () => {
         barras.style.color = "#706bb9";
     }
 });
+
+let alto = navBarr.offsetTop;
+window.addEventListener("scroll", () => {
+    if(window.scrollY >= alto && window.screen.availWidth > 600){
+        navBarr.style.position = "fixed";
+        navBarr.style.top = "0";
+        navBarr.style.left = "0";
+        navBarr.style.width = "97.8%";
+        document.querySelector("section").style.marginTop = "50px";
+    } else {
+        navBarr.style.position = "relative";
+        navBarr.style.width = "auto";
+        document.querySelector("section").style.marginTop = "0px";
+    }
+});
